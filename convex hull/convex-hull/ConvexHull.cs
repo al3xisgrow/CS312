afc301e3667e70062ce4adaddaab2d410f78d358
Trevor Rydalch <trevor.rydalch@outlook.com>
2017-10-05 18:42:00 -0600

    going to work on desktop

---
+++ convex hull/convex-hull/ConvexHull.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace _1_convex_hull
+{
+    class ConvexHull
+    {
+        List<PointF> m_Points;
+        PointF m_leftmost;
+        PointF m_rightmost;
+        PointF m_upperTangentPoint;
+        PointF m_lowerTangentPoint;
+
+        public ConvexHull() { }
+
+        public ConvexHull(List<PointF> _list)
+        {
+            Points = _list;
+            m_leftmost = findLeftMost();
+            m_rightmost = findRightMost();
+        }
+
+        public List<PointF> Points { get => m_Points; set => m_Points = value; }
+        public PointF Leftmost { get => m_leftmost; set => m_leftmost = value; }
+        public PointF Rightmost { get => m_rightmost; set => m_rightmost = value; }
+        public PointF UpperTangentPoint { get => m_upperTangentPoint; set => m_upperTangentPoint = value; }
+        public PointF LowerTangentPoint { get => m_lowerTangentPoint; set => m_lowerTangentPoint = value; }
+
+        private PointF findLeftMost()
+        {
+            PointF leftMost = m_Points[0];
+            for (int i = 0; i < m_Points.Count; i++)
+            {
+                if (m_Points[i].X < leftMost.X)
+                {
+                    leftMost = m_Points[i];
+                }
+            }
+            return leftMost;
+        }
+
+        private PointF findRightMost()
+        {
+            PointF rightMost = m_Points[m_Points.Count-1];
+            for (int i = 0; i < m_Points.Count; i++)
+            {
+                if (m_Points[i].X > rightMost.X)
+                {
+                    rightMost = m_Points[i];
+                }
+            }
+            return rightMost;
+        }
+    }
+}
--- convex hull/convex-hull/ConvexHullSolver.cs
+++ convex hull/convex-hull/ConvexHullSolver.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Linq;
+using _1_convex_hull;
 
 namespace _2_convex_hull
 {
@@ -31,7 +33,182 @@ namespace _2_convex_hull
 
         public void Solve(List<System.Drawing.PointF> pointList)
         {
-            // TODO: Insert your code here
+            List<System.Drawing.PointF> sortedPoints = pointList.OrderBy(o => o.X).ToList();
+            ConvexHull hull = DivideAndConquer(sortedPoints);
+            Draw(hull);            
+        }
+
+        private ConvexHull DivideAndConquer(List<System.Drawing.PointF> p_pointList)
+        {
+            if (p_pointList.Count == 2 || p_pointList.Count == 3)
+            {
+                return new ConvexHull(p_pointList);
+            }
+            else if (p_pointList.Count == 3)
+            {
+                MakeClockwise(p_pointList);
+                return new ConvexHull(p_pointList);
+            }
+            else
+            {
+                int midpoint;
+                ConvexHull leftHull;
+                ConvexHull rightHull;
+
+                if (p_pointList.Count % 2 == 1)
+                {
+                    midpoint = (p_pointList.Count / 2) + 1;
+                    leftHull = DivideAndConquer(p_pointList.GetRange(0, midpoint));
+                    rightHull = DivideAndConquer(p_pointList.GetRange(midpoint, midpoint-1));
+                }
+                else
+                {
+                    midpoint = p_pointList.Count / 2;
+                    leftHull = DivideAndConquer(p_pointList.GetRange(0, midpoint));
+                    rightHull = DivideAndConquer(p_pointList.GetRange(midpoint, midpoint));
+                }
+
+                return Merge(leftHull, rightHull);
+            }
+        }
+
+        private ConvexHull Merge(ConvexHull p_left, ConvexHull p_right)
+        {
+            findUpperTangent(p_left, p_right);
+            findLowerTangent(p_left, p_right);
+            List<PointF> newHullPoints = new List<PointF>();
+
+            int leftIndex = p_left.Points.IndexOf(p_left.UpperTangentPoint);
+            int leftEndIndex = p_left.Points.IndexOf(p_left.LowerTangentPoint);
+            do
+            {
+                newHullPoints.Add(p_left.Points[leftIndex]);
+                if (leftIndex == 0)
+                {
+
+                }
+
+            } while (leftIndex != leftEndIndex);
+
+            int rightIndex = p_right.Points.IndexOf(p_right.UpperTangentPoint);
+            int rightEndIndex = p_right.Points.IndexOf(p_right.LowerTangentPoint);
+            do
+            {
+                newHullPoints.Add(p_right.Points[rightIndex]);
+            } while (rightIndex != rightEndIndex);
+
+            return new ConvexHull(newHullPoints);
+        }
+
+        private void findUpperTangent(ConvexHull p_left, ConvexHull p_right)
+        {
+            Boolean leftPointChange = false;
+            Boolean rightPointChange = false;
+            List<PointF> leftList = p_left.Points;
+            List<PointF> rightList = p_right.Points;
+            PointF leftPoint = p_left.Rightmost;
+            PointF rightPoint = p_right.Leftmost;
+
+            do
+            {
+                float slope = calculateSlope(leftPoint, rightPoint);
+                float checkSlope = slope;
+                for (int i = 0; i < leftList.Count; i++)
+                {
+                    float currSlope = calculateSlope(leftList[i], rightPoint);
+                    if (currSlope < slope)
+                    {
+                        checkSlope = currSlope;
+                        leftPoint = leftList[i];
+                        leftPointChange = true;
+                    }
+                }
+
+                for (int j = 0; j < rightList.Count; j++)
+                {
+                    float currSlope = calculateSlope(leftPoint, rightList[j]);
+                    if (currSlope > slope)
+                    {
+                        checkSlope = currSlope;
+                        rightPoint = rightList[j];
+                        rightPointChange = true;
+                    }
+                }
+                if (checkSlope == slope)
+                {
+                    leftPointChange = rightPointChange = false;
+                }
+            } while (leftPointChange || rightPointChange);
+
+            p_left.UpperTangentPoint = leftPoint;
+            p_right.UpperTangentPoint = rightPoint;
+        }
+        
+        private void findLowerTangent(ConvexHull p_left, ConvexHull p_right)
+        {
+            Boolean leftPointChange = false;
+            Boolean rightPointChange = false;
+            List<PointF> leftList = p_left.Points;
+            List<PointF> rightList = p_right.Points;
+            PointF leftPoint = p_left.Rightmost;
+            PointF rightPoint = p_right.Leftmost;
+
+            do
+            {
+                float slope = calculateSlope(leftPoint, rightPoint);
+                float checkSlope = slope;
+                for (int i = 0; i < leftList.Count; i++)
+                {
+                    float currSlope = calculateSlope(leftList[i], rightPoint);
+                    if (currSlope > slope)
+                    {
+                        checkSlope = currSlope;
+                        leftPoint = leftList[i];
+                        leftPointChange = true;
+                    }
+                }
+
+                for (int j = 0; j < rightList.Count; j++)
+                {
+                    float currSlope = calculateSlope(leftPoint, rightList[j]);
+                    if (currSlope < slope)
+                    {
+                        checkSlope = currSlope;
+                        rightPoint = rightList[j];
+                        rightPointChange = true;
+                    }
+                }
+
+                if (checkSlope == slope)
+                {
+                    leftPointChange = rightPointChange = false;
+                }
+            } while (leftPointChange || rightPointChange);
+
+            p_left.LowerTangentPoint = leftPoint;
+            p_right.LowerTangentPoint = rightPoint;
+            
+        }
+
+        private float calculateSlope(PointF leftPoint, PointF rightPoint)
+        {
+            return ((leftPoint.Y - rightPoint.Y)/(rightPoint.X - leftPoint.X));
+        }
+
+        private void MakeClockwise(List<PointF> p_list)
+        {
+            float slopeOne = calculateSlope(p_list[0], p_list[1]);
+            float slopeTwo = calculateSlope(p_list[0], p_list[2]);
+            if (slopeOne < slopeTwo)
+            {
+                PointF temp = p_list[1];
+                p_list[1] = p_list[2];
+                p_list[2] = temp;
+            }
+        }
+
+        public void Draw(ConvexHull p_hull)
+        {
             throw new NotImplementedException();
         }
     }
